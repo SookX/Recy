ce44ffdaa5116428fb95b47abb83e80ffd167c0c
SookX <vagrivas08@gmail.com>
2024-06-21 23:37:06 +0300

    CRUD operations on Recipes working

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,8 +1,65 @@
 const Recipe = require('../schemas/recipeSchema')
 
-exports.addRecipe = async (req, res) => {
-    const newRecipe = await Recipe.create(req.body)
-    res.status(200).json({
-        message: "Successful"
+const errFunc = (res, err) => {
+    res.status(404).json({
+        message: "Something went wrong!",
+        data: err
     })
+}
+
+const sucFunc = (code, res, data) => {
+    res.status(code).json({
+        message: "Success",
+        data: data
+    })
+}
+
+exports.addRecipe = async (req, res) => {
+    try {
+        const newRecipe = await Recipe.create(req.body)
+        sucFunc(200, res, newRecipe);
+    }catch(err) {
+        errFunc(res, err);
+    }
+
+}
+
+exports.viewRecipes = async (req, res) => {
+    try {
+        const recipes = await Recipe.find();
+        sucFunc(200, res, recipes);
+    }catch(err) {
+        errFunc(res, err);
+    }
+
+}
+
+exports.updateRecipe = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedRecipe = await Recipe.findByIdAndUpdate(id, req.body, {returnDocument: "after"});
+        sucFunc(200, res, updatedRecipe);
+    }catch(err) {
+        errFunc(res, err);
+    }
+}
+
+exports.removeRecipe = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await Recipe.findByIdAndRemove(id);
+        sucFunc(200, res, NaN);
+    }catch(err) {
+        errFunc(res, err);
+    }
+}
+
+exports.viewRecipe = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const recipe = await Recipe.findById(id);
+        sucFunc(200, res, recipe);
+    }catch(err) {
+        errFunc(res, err);
+    }
 }
--- server/routers/recipeRouter.js
+++ server/routers/recipeRouter.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const recipeRouter = express.Router()
 
-recipeRouter.route('/').post(recipeController.addRecipe);
-recipeRouter.route('/:id')
+recipeRouter.route('/').post(recipeController.addRecipe).get(recipeController.viewRecipes);
+recipeRouter.route('/:id').patch(recipeController.updateRecipe).delete(recipeController.removeRecipe).get(recipeController.viewRecipe);
 
 module.exports = recipeRouter;
